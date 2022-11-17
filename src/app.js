4b2b3c0156b2f6d4a246d4f98cb3e269303e684d
santiagokr05@gmail.com <pepoide@gmail.com>
2022-11-17 09:58:37 -0500

    Configuracion y conexion

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const conexionBD = require('./db');
+
+const app = express();
+
+require('dotenv').config();
+
+//Conexion a la DB
+conexionBD();
+
+//Configuraciones
+
+app.set("name", "Hotelia-Api");
+app.set("port", process.env.PORT || 3000);
+app.set("host", process.env.HOST || 'localhost');
