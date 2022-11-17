b065e09c8b6ed7b83d0a391ff833cc785f12a012
KSRL05 <santiagokr05@gmail.com>
2022-11-17 09:59:44 -0500

    Configuracion y conexion

--- src/app.js
+++ src/app.js
@@ -12,4 +12,4 @@ conexionBD();
 
 app.set("name", "Hotelia-Api");
 app.set("port", process.env.PORT || 3000);
-app.set("host", process.env.HOST || 'localhost');
+app.set("host", process.env.HOST || 'lcalhost');
